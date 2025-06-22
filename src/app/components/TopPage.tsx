ac752194751d2d568f11cfda86aa1a494f85b13c
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-06-22 18:09:33 +0900

    awsを構築するapiを追加

--- src/app/components/TopPage.tsx
+++ src/app/components/TopPage.tsx
@@ -1,8 +1,43 @@
 "use client";
+import { useEffect, useState } from "react";
 
 import Link from "next/link";
 
 const TopPage = () => {
+  const [statusMessage, setStatusMessage] = useState<string | null>(
+    "ビルド実行済み"
+  );
+  // useEffect(() => {
+  //   const BuildMyBlogApi = async () => {
+  //     try {
+  //       const res = await fetch(
+  //         "https://2qsi7kz2ul.execute-api.ap-northeast-1.amazonaws.com/prod/start-build-my-blog",
+  //         {
+  //           method: "POST",
+  //           headers: {
+  //             "Content-Type": "application/json",
+  //           },
+  //         }
+  //       );
+  //       const data = await res.json();
+
+  //       if (data.status === "done") {
+  //         setStatusMessage("構築完了");
+  //       } else if (data.status === "starting") {
+  //         setStatusMessage("構築開始");
+  //       } else if (data.status === "running") {
+  //         setStatusMessage("構築中");
+  //       } else {
+  //         console.log("不明なステータス:", data.status);
+  //       }
+  //     } catch (err) {
+  //       console.error("API call failed:", err);
+  //     }
+  //   };
+
+  //   BuildMyBlogApi();
+  // }, []);
+
   return (
     <div className=" top-0 left-0 z-20 pointer-events-none inline-block">
       <h1 className="m-4 font-bold text-6xl text-white pointer-events-auto">
@@ -28,6 +63,7 @@ const TopPage = () => {
           アーカイブ
         </Link>
       </ul>
+      <p className="text-white">{statusMessage}</p>
     </div>
   );
 };
