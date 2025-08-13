1e54ea5ee6e6bbf0154fdc3e46548a9e0eca19d4
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-08-13 11:35:05 +0900

    レスポンシブ対応

--- src/app/components/TopPage.tsx
+++ src/app/components/TopPage.tsx
@@ -1,32 +1,16 @@
 "use client";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 import Link from "next/link";
+// import { startBuild } from "../lib/api/startBuild";
 
 const TopPage = () => {
-  const [statusMessage, setStatusMessage] = useState<string | null>();
-  // "ビルド実行済み"
   // useEffect(() => {
   //   const BuildMyBlogApi = async () => {
-  //     try {
-  //       const res = await fetch(
-  //         "https://2qsi7kz2ul.execute-api.ap-northeast-1.amazonaws.com/prod/start-build-my-blog",
-  //         {
-  //           method: "POST",
-  //           headers: {
-  //             "Content-Type": "application/json",
-  //           },
-  //         }
-  //       );
-  //       const data = await res.json();
 
-  //       if (data.status === "done") {
-  //         setStatusMessage("構築完了");
-  //       } else if (data.status === "starting") {
-  //         setStatusMessage("構築中");
-  //       } else {
-  //         console.log("不明なステータス:", data.status);
-  //       }
+  //     try {
+  //       const data = await startBuild();
+  //       console.log(data.status);
   //     } catch (err) {
   //       console.error("API call failed:", err);
   //     }
@@ -37,33 +21,32 @@ const TopPage = () => {
 
   return (
     <div className="left-0  pointer-events-none inline-block relative z-20">
-        <div className="font-semibold text-8xl text-white  ">
-          <h1 >Takeda Syuri</h1>
-        </div>
-        <div>
-          <ul className="ml-4 mt-20 grid gap-6 text-5xl font-semibold z-20 text-white  pointer-events-auto ">
-            <Link
-              href="profile "
-              className="hover:text-orange-400 hover:scale-110 duration-500 z-20  w-fit"
-            >
-              プロフィール
-            </Link>
-            <Link
-              href="portfolio"
-              className="hover:text-orange-400 hover:scale-110 duration-500   z-20   w-fit"
-            >
-              ポートフォリオ
-            </Link>
-            <Link
-              href="record"
-              className="hover:text-orange-400 hover:scale-110 duration-500 z-20 w-fit"
-            >
-              アーカイブ
-            </Link>
-          </ul>
-        </div>
-        <p className="text-white">{statusMessage}</p>
+      <div className="text-5xl md:text-9xl font-bold  text-white  ">
+        <h1>Takeda Syuri</h1>
       </div>
+      <div>
+        <ul className="ml-4 mt-20 grid gap-6 text-3xl md:text-5xl font-semibold z-20 text-white  pointer-events-auto ">
+          <Link
+            href="profile "
+            className="hover:text-orange-400 hover:scale-110 duration-500 z-20  w-fit"
+          >
+            プロフィール
+          </Link>
+          <Link
+            href="portfolio"
+            className="hover:text-orange-400 hover:scale-110 duration-500   z-20   w-fit"
+          >
+            ポートフォリオ
+          </Link>
+          <Link
+            href="record"
+            className="hover:text-orange-400 hover:scale-110 duration-500 z-20 w-fit"
+          >
+            アーカイブ
+          </Link>
+        </ul>
+      </div>
+    </div>
   );
 };
 
