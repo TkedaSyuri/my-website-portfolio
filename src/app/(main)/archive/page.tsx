b245aeb8883a9a4910034898af6d234a97327ee5
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-08-24 17:59:59 +0900

    レイアウトを変更

--- src/app/(main)/archive/page.tsx
+++ src/app/(main)/archive/page.tsx
@@ -79,27 +79,35 @@ const Data = [
 
 const ArchivePage = () => {
   return (
-      <div className="relative z-20 pt-10  overflow-y-auto max-h-[95vh] scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-gray-300">
-        {/* このページの説明 */}
-        <p className="text-base font-bold text-white mb-4 text-center ">
-          このページでは、これまでに学習してきたプログラミング言語、フレームワーク、ライブラリなどの技術を一覧形式で紹介しています。
-          途中まで取り組んだものも含めて掲載しています
-        </p>
+    <div className="min-h-screen flex items-center justify-center relative z-20 ">
+      <div
+        className="text-white max-w-6xl w-full p-2 rounded-2xl shadow-xl backdrop-blur-xs
+        overflow-y-auto max-h-[80vh] scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-500"
+      >
         <div className="flex justify-center">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 ">
+        <p className="text-sm md:text-base font-bold pb-4 text-white ">
+          このページでは、これまでに学習してきたプログラミング言語、フレームワーク、ライブラリなどの技術を一覧形式で紹介しています。
+          途中まで取り組んだものも含めて掲載しています。
+        </p>
+
+        </div>
+        <div className="flex justify-center">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 ">
             {Data.map((items) => (
               <div
                 key={items.id}
-                className="flex flex-col items-center  mt-2 p-2 m-2  w-96 bg-gray-200 text-black  border-blue-500 border rounded  "
+                className="flex flex-col items-center p-3 bg-gray-100 text-gray-800 border border-gray-300 rounded-xl shadow-md hover:shadow-xl hover:border-blue-400 transition duration-300"
               >
-                <p className="text-2xl text-center">「{items.title}」</p>
-                <div className=" flex flex-col justify-center items-center break-all ">
-                  <p className="mr-1 text-sm ">Git Hubへのリンク</p>
+                <p className="text-xl font-bold text-center mb-2">
+                  {items.title}
+                </p>
+                <div className="flex flex-col items-center text-sm text-gray-600">
+                  <p className="mb-1">GitHub リポジトリ</p>
                   <Link
                     href={items.url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-blue-800 underline hover:text-blue-400 transition duration-200"
+                    className="text-blue-700 underline hover:text-blue-500 transition-colors"
                   >
                     {items.url}
                   </Link>
@@ -109,6 +117,7 @@ const ArchivePage = () => {
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
