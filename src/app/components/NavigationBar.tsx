91b76d72f187e6b00cab00ff7880faed122ccf18
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-08-24 17:38:45 +0900

    ホバー時のサイズを 微調整

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -12,38 +12,38 @@ const NavigationBar = () => {
       <div className="flex justify-end pt-3">
         <div className="flex text-white w-fit cursor-default">
           <Link href="/" className="flex flex-col justify-center items-center gap-1 w-25 group">
-            <div className="group-hover:text-orange-500 group-hover:scale-150 transition-transform duration-500">
+            <div className="group-hover:text-orange-500 group-hover:scale-120 transition-transform duration-700">
               <BsFire size={20} />
             </div>
-            <p       className="text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+            <p       className="text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-700"
             >
               ホーム
             </p>
           </Link>
 
           <Link href="/profile"  className="flex flex-col justify-center items-center gap-1 w-25   group">
-            <div className="group-hover:text-blue-500 group-hover:scale-150 transition-transform duration-500">
+            <div className="group-hover:text-blue-500 group-hover:scale-120 transition-transform duration-700">
               <CgProfile size={20} />
             </div>
-            <p className="text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+            <p className="text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-700">
               プロフィール
             </p>
           </Link>
 
           <Link href="/portfolio"  className="flex flex-col justify-center items-center gap-1 w-25  group">
-            <div className="group-hover:text-green-500 group-hover:scale-150 transition-transform duration-500">
+            <div className="group-hover:text-green-500 group-hover:scale-120 transition-transform duration-700">
               <FaLaptopCode size={20} />
             </div>
-            <p  className="text-sm opacity-0 group-hover:opacity-100  transition-opacity duration-500">
+            <p  className="text-sm opacity-0 group-hover:opacity-100  transition-opacity duration-700">
               ポートフォリオ
             </p>
           </Link>
 
           <Link href="/archive" className="flex flex-col justify-center items-center gap-1 w-25  group">
-            <div className="group-hover:text-cyan-500 group-hover:scale-150 transition-transform duration-500">
+            <div className="group-hover:text-cyan-500 group-hover:scale-120 transition-transform duration-700">
               <FaBoxArchive size={20} />
             </div>
-            <p className="text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+            <p className="text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-700">
               アーカイブ
             </p>
           </Link>
