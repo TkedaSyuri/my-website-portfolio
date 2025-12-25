4193808a169e7727ab680802d60ff318f74a1dce
Takeda Syuri <30nanchikusyuri@gmail.com>
2025-12-25 22:14:37 +0900

    タイトルを変更

--- src/app/components/TopPage.tsx
+++ src/app/components/TopPage.tsx
@@ -1,29 +1,13 @@
 "use client";
-// import { useEffect, useState } from "react";
 
 import Link from "next/link";
-// import { startBuild } from "../lib/api/startBuild";
 
 const TopPage = () => {
-  // useEffect(() => {
-  //   const BuildMyBlogApi = async () => {
-
-  //     try {
-  //       const data = await startBuild();
-  //       console.log(data.status);
-  //     } catch (err) {
-  //       console.error("API call failed:", err);
-  //     }
-  //   };
-
-  //   BuildMyBlogApi();
-  // }, []);
-
   return (
     <div className="left-0  pointer-events-none inline-block relative z-20">
       <div className="text-6xl md:text-8xl font-bold  text-white  ">
-        <h1>Takeda Syuri<span className="sr-only">武田珠里 ホームページ</span></h1>
-      </div>
+        <h1>Takeda Syuri <span className="text-6xl">ーポートフォリオー</span><span className="sr-only">武田珠里 ホームページ</span></h1>
+      </div>c
       <div>
         <ul className="ml-4 mt-20 grid gap-6 text-4xl md:text-5xl font-semibold z-20 text-white  pointer-events-auto ">
           <Link
